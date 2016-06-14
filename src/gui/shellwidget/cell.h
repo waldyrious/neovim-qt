e220cbdbdd0182bd48af6f77cd05eb98707dfaec
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-06-14 16:30:00 +0100

    Store low surrogate in shell contents
    When placing unicode surrogates into the shell we need to keep both
    chars in the same cell. Cell::text() now returns either the cell
    character or both chars if the lower surrogate was set using
    Cell::setLowSurrogate().

--- src/gui/shellwidget/cell.h
+++ src/gui/shellwidget/cell.h
@@ -45,6 +45,18 @@ public:
 		doubleWidth = (konsole_wcwidth(c.unicode()) > 1);
 	}
 
+	inline void setLowSurrogate(const QChar& chr) {
+		lowSurrogate = chr;
+	}
+
+	inline QString text() const {
+		if (c.isHighSurrogate() && lowSurrogate.isLowSurrogate()) {
+			return QString(c) + lowSurrogate;
+		} else {
+			return c;
+		}
+	}
+
 	static inline Cell invalid() {
 		Cell c = Cell('X', Qt::white, Qt::red, QColor(), false, false, false, false);
 		c.valid = false;
@@ -52,6 +64,7 @@ public:
 	}
 
 	QChar c;
+	QChar lowSurrogate;
 	QColor foregroundColor, backgroundColor, specialColor;
 	bool bold, italic, underline, undercurl;
 	bool valid;
